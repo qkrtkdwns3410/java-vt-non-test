00b34640589b75020671a1f0ed9ef2f94d1c0a9d
psj <qkrtkdwns3410@gmail.com>
2025-10-05 15:32:31 +0900

    Initial commit

---
+++ src/main/java/com/example/javavtnontest/JavaVtNonTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.javavtnontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaVtNonTestApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(JavaVtNonTestApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/example/javavtnontest/JavaVtNonTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javavtnontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaVtNonTestApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
